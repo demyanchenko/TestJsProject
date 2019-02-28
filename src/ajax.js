01ccf72068a10128b0923efd7ea5a4189980a455
demyanchenko <demyanchenko.ao@gmail.com>
2019-02-28 11:01:41 +0300

    Сделали webpack, ajax

--- src/ajax.js
+++ src/ajax.js
@@ -1,18 +1,33 @@
-new Promise((resolve, reject) => {
-  const xmlHttpRequest = new XMLHttpRequest();
-  xmlHttpRequest.onreadystatechange = () => {
-    if (xmlHttpRequest.readyState === 4) {
-      if (xmlHttpRequest.status === 200) {
-        resolve(xmlHttpRequest.text);
-      } else {
-        reject(xmlHttpRequest);
+function getPromise() {
+  return new Promise((resolve, reject) => {
+    const xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = () => {
+      if (xmlHttpRequest.readyState === 4) {
+        if (xmlHttpRequest.status === 200) {
+          resolve(xmlHttpRequest.text);
+        } else {
+          reject(xmlHttpRequest);
+        }
       }
-    }
-  };
-  xmlHttpRequest.open('GET', 'localhosxt:8080/1');
-  xmlHttpRequest.send(null);
-}).then(text => {
-  const { id, price } = JSON.parse(text);
-  return id + price;
-}).then(value => console.log(value))
+    };
+    xmlHttpRequest.open('GET', 'localhosxt:8080/1');
+    xmlHttpRequest.send(null);
+  });
+}
+
+// Используем async вместо Promise
+async function show() {
+  const text = await getPromise();
+  const {id, price} = JSON.parse(text);
+  console.log(id + price);
+}
+
+show()
   .catch(reason => console.log(reason.status));
+
+// Использование Promise
+/* getPromise().then(text => {
+   const { id, price } = JSON.parse(text);
+   return id + price;
+ }).then(value => console.log(value))
+   .catch(reason => console.log(reason.status));*/
